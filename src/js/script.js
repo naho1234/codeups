008da3f2fe01a02c1d8b26c2df4471cd4a9422f0
naho1234 <naa01.1105ily@gmail.com>
2023-08-29 10:22:33 +0900

    [add]8/29 キャンペーン終了

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,75 @@
+window.addEventListener("load", function () {
+  // ローディングアニメーションを表示
+  $(".loader").fadeIn(600, function () {
+    $(".text, .subText").fadeIn(300);
+  });
+
+  // ローディングアニメーションが終わったら
+  setTimeout(function () {
+    $(".loader").fadeOut(400, function () {
+			 $(this).remove();
+      // 1枚目の右側の画像を元の位置に移動
+      $(".mv-splide__slide:first .mv-img--right").animate(
+        {
+          top: "0px",
+          right: "0px",
+        },
+        800
+      ),
+        $(".mv-splide__slide:first .mv-img--left").animate(
+          {
+            top: "0px",
+            left: "0px",
+          },
+          800
+        );
+
+      // Splideの初期化
+      const mvSplide = new Splide(".mv-splide", {
+        type: "fade",
+        rewind: true,
+        autoplay: true,
+        interval: 7000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        arrows: false,
+        pagination: false,
+        drag: false,
+      });
+
+      // Splideがマウントされたとき（初期化が完了したとき）にテキストをフェードイン
+      mvSplide.on("mounted", function () {
+        $(".mv-splide__slide.is-active .text, .mv-splide__slide.is-active .subText").delay(1000).fadeIn(400);
+      });
+
+      // Splideのイベントリスナー
+      mvSplide.on("active", (slide) => {
+        $(slide.slide).find(".text, .subText").hide().delay(1000).fadeIn(400);
+      });
+      const campaignSplide = new Splide(".campaign-splide", {
+        type: "slide",
+        arrows: true,
+        pagination: false,
+        autoplay: true,
+        rewind: true,
+        start: 0,
+        drag: true,
+      });
+      mvSplide.mount();
+      campaignSplide.mount();
+    });
+  }, 5000);
+});
+
 jQuery(function ($) {
   //ナビバートグル
   $(".js-hamburger").on("click", function () {
     if ($(".js-hamburger").hasClass("is-open")) {
-      // $('.js-drawer-menu').fadeOut();
+			$("body").removeClass("no-scroll");
       $(".js-drawer-menu").removeClass("is-open");
       $(this).removeClass("is-open");
     } else {
-      // $('.js-drawer-menu').fadeIn();
+			$("body").addClass("no-scroll");
       $(".js-drawer-menu").addClass("is-open");
       $(this).addClass("is-open");
     }
